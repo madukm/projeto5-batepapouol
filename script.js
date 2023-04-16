0244453aeecf10025a5b25e2e87816ddc88b3390
Madu <kawakamimadu@gmail.com>
2023-04-16 13:41:52 -0300

    Feat - User connection and check connection

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
-axios.defaults.headers.common['Authorization'] = 'VpIoDEaMTUNu16xPH1ZhJhpF';
-
 let userName, loggedIn;
 
-do{
-    loggedIn = enterRoom();
-}while(!loggedIn);
+
+axios.defaults.headers.common['Authorization'] = 'U3hR3vKhBRjD3SsSokNzrRvU';
+
+loggedIn = enterRoom();
+
+const connectionInterval = setInterval(checkConnection, 5000);
 
 function enterRoom() {
     userName = String(prompt('Qual seu nome?'));
-    axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', {
+    const promise = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants',
+    {
         name: userName
-    })
-    .then((response) => {
-        login();
-        return true;
-    }, (error) => {
-        alert('Já existe um usuário com este nome');
-        return false;
     });
+    promise.then(login); 
+    promise.catch(failLogin);
 
-    
 };
 
+function login(){
+    alert("Você entrou na sala!");
+}
 
+function failLogin(){
+    if(erro.response.status === 400) {
+        alert('Usuário já logado. Tente novamente');
+    }
+    location.reload();
+}
+
+function checkConnection(){
+    const promise = axios.post('https://mock-api.driven.com.br/api/vm/uol/status',
+    {
+        name: userName
+    });
+    promise.then(connected);
+    promise.catch(disconnected);
+}
+
+function connected(){
+    console.log("still connected");
+}
+
+function disconnected(){
+    alert("Você se desconectou!");
+}
 
     
