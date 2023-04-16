3462cc12777c5eac6d34c165411e769b020b6635
Madu <kawakamimadu@gmail.com>
2023-04-16 19:06:44 -0300

    Updating chat and sending message

--- script.js
+++ script.js
@@ -1,49 +1,101 @@
-let userName, loggedIn;
+let userName;
 
 
-axios.defaults.headers.common['Authorization'] = 'U3hR3vKhBRjD3SsSokNzrRvU';
+axios.defaults.headers.common['Authorization'] = 'BgNaMGjpbxSXb08gFSR5X5hg';
 
-loggedIn = enterRoom();
+login();
 
 const connectionInterval = setInterval(checkConnection, 5000);
 
-function enterRoom() {
-    userName = String(prompt('Qual seu nome?'));
-    const promise = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants',
-    {
-        name: userName
-    });
-    promise.then(login); 
-    promise.catch(failLogin);
 
-};
-
-function login(){
-    alert("Você entrou na sala!");
-}
-
-function failLogin(){
-    if(erro.response.status === 400) {
-        alert('Usuário já logado. Tente novamente');
+async function login() {
+    try {
+        userName = String(prompt('Qual seu nome?'));    
+        const res = await axios.post('https://mock-api.driven.com.br/api/vm/uol/participants',
+        {
+            name: userName
+        });
+        let data = res.data;
+        alert("Você está conectado!");
+        updateChat();
+        return true;
+    } catch (error) {
+        console.log(error);
+        if(error.response.status === 400){
+            alert('Usuário já logado. Tente novamente');
+            window.location.reload();
+            console.log("Usuário já logado.");
+        }
+        return false;
     }
-    location.reload();
 }
 
-function checkConnection(){
-    const promise = axios.post('https://mock-api.driven.com.br/api/vm/uol/status',
-    {
-        name: userName
-    });
-    promise.then(connected);
-    promise.catch(disconnected);
+
+async function checkConnection(){
+    try{
+        const res = await axios.post('https://mock-api.driven.com.br/api/vm/uol/status',
+        {
+            name: userName
+        });
+        console.log("still connected");
+        let data = res.data;;
+        updateChat();
+        return data;
+    } catch(error){
+        alert("Você foi desconectado");
+        window.location.reload();
+        return error.response;
+    }
 }
 
-function connected(){
-    console.log("still connected");
+function updateChat(){
+    const promise = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
+    promise.then(displayMessages);
 }
 
-function disconnected(){
-    alert("Você se desconectou!");
+function displayMessages(res){
+    res = res.data;
+    for(let i=0; i<100; i++){
+        displaySingleMessage(res[i]);
+    }
+    window.scrollTo(0, document.body.scrollHeight);
 }
 
+function displaySingleMessage(res){
+    const container = document.querySelector('.chat-container');
+    const message = document.createElement('div');
+    message.classList.add('message');
+    message.innerHTML = "";
+    message.innerHTML += `
+        <span class="time"> ${res.time} </span>
+        ` + "&nbsp" + `<span class="bold"> ${res.from} </span>
+        <span> para <span/>
+        <span class="bold"> ${res.to}: </span>
+        <span> ${res.text}</span>
+    `;
+    if(res.type === "status"){
+        message.classList.add('status');
+    }
+    container.appendChild(message);
+}
+
+async function getMessageText(){
+    const userInput = document.getElementById("messageText");
     
+    try {
+        const res = await axios.post('https://mock-api.driven.com.br/api/vm/uol/messages',
+        {
+            from: userName,
+            to: "Todos",
+            text: userInput.value,
+            type: "message"
+        });
+        let data = res.data;
+        userInput.value = "";
+        updateChat();
+        return data;
+    } catch (error) {
+        console.log(error);
+        return error.response;
+    }
+}
